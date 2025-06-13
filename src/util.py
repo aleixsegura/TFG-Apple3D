06e18855a5598b610d362d627a6f2e9b86819df3
Aleix Segura <83470660+aleixsegura@users.noreply.github.com>
2025-06-13 18:59:35 +0200

    Delete src directory

--- src/util.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-from numpy import float64
-from pyproj import Transformer
-
-def latlon_to_utm(longitude: float64, latitude: float64) -> Tuple[float64, float64]:
-    transformer = Transformer.from_crs('epsg:4326', 'epsg:25831', always_xy=True)
-    easting, northing = transformer.transform(longitude, latitude)
-    
-    return float64(easting), float64(northing)
